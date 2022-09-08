97a996b800f9f3125c6882e7ac05a4467f85505c
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-09-08 11:01:51 -0700

    Implemented SudokuSolver.validate method, and unit tests for valid and invalid puzzle strings

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,29 @@
 class SudokuSolver {
-
+  // Determines if a given puzzleString is valid i.e.:
+  // contains 81 characters from 1-9 or '.'
+  // Returns an array with the first item boolean - whether puzzle is valid or not
+  // If puzzle is not valid, the second item in the return array is an error object
+  // NOTE: Does not check if number placements inside grid are valid or conflict
   validate(puzzleString) {
+    if (puzzleString.length !== 81) {
+      return [false, { error: 'Expected puzzle to be 81 characters long' }];
+    }
+
+    // Otherwise check that string only contains valid characters:
+    if (!/[1-9\.]{81}/.test(puzzleString)) {
+      return [false, { error: 'Invalid characters in puzzle' }];
+    }
+
+    return [true, {}];
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {
+  checkRowPlacement(puzzleString, row, column, value) {}
 
-  }
+  checkColPlacement(puzzleString, row, column, value) {}
 
-  checkColPlacement(puzzleString, row, column, value) {
+  checkRegionPlacement(puzzleString, row, column, value) {}
 
-  }
-
-  checkRegionPlacement(puzzleString, row, column, value) {
-
-  }
-
-  solve(puzzleString) {
-    
-  }
+  solve(puzzleString) {}
 }
 
 module.exports = SudokuSolver;
-
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,8 +2,92 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+const solver = new Solver();
+
+const puzzlesAndSolutions = require('../controllers/puzzle-strings');
 
 suite('Unit Tests', () => {
+  suite('SudokuSolver.validate Tests', () => {
+    test('SudokuSolver.validate validates valid puzzleStrings', () => {
+      let valid, err;
 
+      [valid, err] = solver.validate('.'.repeat(81));
+      assert.isTrue(valid, 'puzzleString should be validated');
+      assert.deepEqual(err, {}, 'No error should be returned');
+
+      [valid, err] = solver.validate(
+        '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..',
+      );
+      assert.isTrue(valid, 'puzzleString should be validated');
+      assert.deepEqual(err, {}, 'No error should be returned');
+
+      for (const [puzzleStr, solution] of puzzlesAndSolutions) {
+        [valid, err] = solver.validate(puzzleStr);
+        assert.isTrue(valid, 'puzzleString should be validated');
+        assert.deepEqual(err, {}, 'No error should be returned');
+      }
+    });
+
+    test('SudokuSolver.validate returns length error when puzzleString has wrong length ', () => {
+      const expectedError = {
+        error: 'Expected puzzle to be 81 characters long',
+      };
+      let valid, err;
+
+      [valid, err] = solver.validate('.');
+      assert.isFalse(valid, 'puzzleString is not valid');
+      assert.deepEqual(
+        err,
+        expectedError,
+        'Incorrect puzzle length error should be returned',
+      );
+
+      [valid, err] = solver.validate('.1234'.repeat(16)); // 80 characters
+      assert.isFalse(valid, 'puzzleString is not valid');
+      assert.deepEqual(
+        err,
+        expectedError,
+        'Incorrect puzzle length error should be returned',
+      );
+
+      [valid, err] = solver.validate('.1234'.repeat(16) + '12'); // 82 characters
+      assert.isFalse(valid, 'puzzleString is not valid');
+      assert.deepEqual(
+        err,
+        expectedError,
+        'Incorrect puzzle length error should be returned',
+      );
+
+      [valid, err] = solver.validate('.123d'.repeat(16) + 'ab'); // 82 characters, with some invalid characters
+      assert.isFalse(valid, 'puzzleString is not valid');
+      assert.deepEqual(
+        err,
+        expectedError,
+        'Incorrect puzzle length error should be returned',
+      );
+    });
+
+    test('SudokuSolver.validate returns character error when puzzleString has correct length but invalid characters ', () => {
+      const expectedError = {
+        error: 'Invalid characters in puzzle',
+      };
+      let valid, err;
+
+      [valid, err] = solver.validate('a'.repeat(81));
+      assert.isFalse(valid, 'puzzleString is not valid');
+      assert.deepEqual(
+        err,
+        expectedError,
+        'Invalid puzzle characters error should be returned',
+      );
+
+      [valid, err] = solver.validate('.'.repeat(80) + '0');
+      assert.isFalse(valid, 'puzzleString is not valid');
+      assert.deepEqual(
+        err,
+        expectedError,
+        'Invalid puzzle characters error should be returned',
+      );
+    });
+  });
 });
